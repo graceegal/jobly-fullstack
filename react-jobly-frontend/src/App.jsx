e569d2185de5768f265cb8d9c0ad09abb2b5d01b
Gracee Gallivan <gracee.gallivan@gmail.com>
2024-04-09 13:59:50 -0700

    initial commit

---
+++ react-jobly-frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import logo from './logo.svg'
+import './App.css';
+
+
+/** Component for entire page.
+ *
+ * Props: none
+ * State: none
+ *
+*/
+
+function App() {
+
+};
+
+export default App;
