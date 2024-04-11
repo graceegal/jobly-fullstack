875e11b856deb152d540e8b8cb7c0ad9ee116ead
David Sapiro <dsap48@gmail.com>
2024-04-11 10:14:36 -0700

    added userContext and started App functionality

--- react-jobly-frontend/src/App.jsx
+++ react-jobly-frontend/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import RoutesList from './RoutesList';
 import Navigation from './Navigation';
+import { useEffect, useState } from 'react';
+import userContext from "./userContext";
 
 /** Component for entire page.
  *
@@ -14,17 +16,50 @@ import Navigation from './Navigation';
 */
 
 function App() {
-  console.log("Rendered App");
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Navigation />
-        <div>
-          <RoutesList />
+    const [currUser, setCurrUser] = useState({ userData: null, isLoading: true });
+    const [token, setToken] = useState({token: null, isLoading: true});
+    const [loginData, setLoginData] = useState(null);
+    const [signupData, setSignupData] = useState(null);
+    console.log("Rendered App");
+
+
+    // get user data from handleAuth
+    // update login/signup Data state
+    // useEffect uses user data to make API login/signup call
+    // if good credentials, update context
+    // if bad credentials, set errors, don't update context, clear user state
+
+    /** Sets loginData state with user inputs from login form */
+    function handleLogin(formData) {
+        setLoginData(formData);
+    }
+
+    /** Sets signupData state with user inputs from signup form */
+    function handleSignup(formData) {
+        setSignupData(formData);
+    }
+
+    async function signup() {
+
+    }
+
+
+
+
+
+
+    return (
+        <div className="App">
+            <userContext.Provider value={{ user: currUser }}>
+                <BrowserRouter>
+                    <Navigation />
+                    <div>
+                        <RoutesList />
+                    </div>
+                </BrowserRouter>
+            </userContext.Provider>
         </div>
-      </BrowserRouter>
-    </div>
-  );
+    );
 };
 
 export default App;
