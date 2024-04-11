1a471f2da371b61c9fd70a5f7cfd31ba59e4710f
Gracee Gallivan <gracee.gallivan@gmail.com>
2024-04-11 12:34:25 -0700

    updated nav bar with new links

--- react-jobly-frontend/src/Navigation.jsx
+++ react-jobly-frontend/src/Navigation.jsx
@@ -1,4 +1,6 @@
 import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import userContext from "./userContext";
 import "./Navigation.css";
 
 /** Navigation Bar.
@@ -13,14 +15,29 @@ import "./Navigation.css";
 
 function Navigation() {
     console.log("Rendered Navigation");
+
+    const { user } = useContext(userContext);
+    console.log("user from context", user);
+    // access context to see if there is a currUser
+    // if no curr User - show Jobly, signup and login
+    // if curr user, show Jobly, companies, jobs, profile, logout
     return (
         <nav className="Navigation navbar bg-body-tertiary">
             <div className="Navigation-home container-fluid">
                 <NavLink className="active navbar-brand" to="/" >Jobly</NavLink>
-                <NavLink className="nav-link" to="/companies" end>Companies</NavLink>
-                <NavLink className="nav-link" to="/jobs" end>Jobs</NavLink>
+                {user.username
+                    ?   <div className="right-nav d-flex">
+                            < NavLink className="nav-link ms-2" to="/jobs" end>Jobs</NavLink>
+                            <NavLink className="nav-link ms-2" to="/companies" end>Companies</NavLink>
+                            <NavLink className="nav-link ms-2" to="/profile" end>Profile</NavLink>
+                        </div>
+                    :   <div className="right-nav d-flex">
+                            <NavLink className="nav-link ms-2" to="/signup" end>Sign Up</NavLink>
+                            <NavLink className="nav-link ms-2" to="/login" end>Login</NavLink>
+                        </div>
+                }
             </div>
-        </nav>
+        </nav >
     );
 }
 
