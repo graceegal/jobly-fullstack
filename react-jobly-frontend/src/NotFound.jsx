2c760ec4b5750212ea6812efd81a36b8a166c194
Gracee Gallivan <gracee.gallivan@gmail.com>
2024-04-12 15:43:20 -0700

    formatted Not Found page

--- react-jobly-frontend/src/NotFound.jsx
+++ react-jobly-frontend/src/NotFound.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 /** Component for Not Found 404.
  *
- * Props: none
+ * Props: "message"
  *
  * State: none
  *
@@ -11,9 +11,11 @@ import { Link } from "react-router-dom";
 function NotFound({ message = "Not Found" }) {
     console.log("Rendered NotFound");
     return (
-        <div className="NotFound">
-            <p>{message}</p>
-            <Link className="btn btn-primary" to={"/"} >Back to home</Link>
+        <div className="container">
+            <div className="NotFound mt-5 text-center">
+                <h1 className="mb-4">{message}</h1>
+                <Link className="btn btn-primary mt-2 ms-2" to={"/"} >Back to home</Link>
+            </div>
         </div>
     );
 };
